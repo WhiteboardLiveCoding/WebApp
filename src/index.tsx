4cfe0cf2397de30bde00ca20f947b7a6c8afa1b5
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2017-11-20 23:46:05 +0000

    Update root app and index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { FocusStyleManager } from '@blueprintjs/core';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
+
+FocusStyleManager.onlyShowFocusOnTabs();
+
 registerServiceWorker();
